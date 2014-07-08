2854193d7c825f226fb6f7309c22b8e64573b6c8
hallfox <hallfox>
2014-07-08 10:13:40 -0400

    initial commit

---
+++ begingame/hallfox/game_over2.cpp
@@ -0,0 +1,18 @@
+//Game Over 2.0
+//Demonstrating a using directive
+
+//a little include statement
+#include <iostream>
+//new! this allows us to get all the stuff out of std
+using namespace std;
+
+/*main function being declared, traditionally returns 0 if all
+  goes well
+*/
+int main() //no paramaters yet
+{
+  //same as before, with less typing
+  cout << "Game Over!" << endl;
+  //all is good with the world
+  return 0;
+}
---
+++ begingame/hallfox/game_over3.cpp
@@ -0,0 +1,13 @@
+//Game Over 3.0
+//Demonstrates using declarations
+
+#include <iostream>
+using std::cout; //specifies what we need from std
+using std::endl; //in general it's a bit clearer to read
+
+int main()
+{
+  //same as before
+  cout << "Game Over!" << endl;
+  return 0;
+}
