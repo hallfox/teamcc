475bf5504f61f4e6a9e29c6fcc7c11fb592ed132
hallfox <hallfox>
2014-07-08 13:43:49 -0400

    doc clang 1.1

--- clang/hallfox/helloworld.c
+++ clang/hallfox/helloworld.c
@@ -1,9 +1,18 @@
-//header for standard input output library
-#include <stdio.h>
+/*header for standard input output library
+ will recognize if terminating > is left out
+will raise a filename error if either < or both are missing
+will compile and run if <> are replaced with ""*/
+#include "stdio.h"
 
-/* the main function; every program must contain a main
-   function as every program starts here */
-main() //no args
+/* the main function; 
+every program must contain a main function as every program
+starts here;
+will expect a symbol other than () if () are deleted;
+will raise a cygwin error if main is not defined;
+apparently, if you compile with the -Wall flag, the compiler
+will warn that main is by default an int function and must
+return 0;*/
+main() //no args, no type
 {
   /*printf is a function that prints strings to the console
   need the string to be on one line
